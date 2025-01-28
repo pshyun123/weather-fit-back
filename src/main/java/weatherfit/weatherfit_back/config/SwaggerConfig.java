a8b6ecfe1d22f28325ecc5f7b55b0109aa2ce384
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2025-01-28 21:14:09 +0900

    swagger add

---
+++ src/main/java/weatherfit/weatherfit_back/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package weatherfit.weatherfit_back.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Springboot Open Api Test with Swagger")
+                .description("설명 부분")
+                .version("1.0.0");
+    }
+}
