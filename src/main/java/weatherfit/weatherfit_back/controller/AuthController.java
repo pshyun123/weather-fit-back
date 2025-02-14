c83caea63dcfb7fc437a938bcd67d1bf8ea4ab27
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-02-14 17:18:41 +0900

    login

--- src/main/java/weatherfit/weatherfit_back/controller/AuthController.java
+++ src/main/java/weatherfit/weatherfit_back/controller/AuthController.java
@@ -1,7 +1,7 @@
 package weatherfit.weatherfit_back.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
